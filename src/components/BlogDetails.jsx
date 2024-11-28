e2651074add06dcc84c4364fb82eaccaeca54def
justinjensen <justinjensen@users.noreply.github.com>
2024-11-27 23:40:13 -0700

    exercise 5.8

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
+import blogService from '../services/blogs'
+
 const BlogDetails = ({ blog }) => {
+  const [likes, setLikes] = useState(blog.likes)
+
+  const handleLike = async () => {
+    const updatedBlog = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: likes + 1
+    }
+    await blogService.update(blog.id, updatedBlog)
+    setLikes(likes + 1)
+  }
+
   return (
     <div>
       <div>{blog.url}</div>
       <div>
-        {blog.likes} likes <button>like</button>
+        {likes} likes <button onClick={handleLike}>like</button>
       </div>
       <div>{blog.user.name}</div>
     </div>
